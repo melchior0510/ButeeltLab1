fe60c5c12bb33b37006609639e187d47f0df162f
melchior0510 <melchior0510@gmail.com>
2026-02-11 15:00:32 +0800

    initial commit of lab1

---
+++ src/main/java/com/lab1/App.java
@@ -0,0 +1,19 @@
+package com.lab1;
+
+/**
+ * Hello world!
+ *
+ */
+public final class App {
+    private App() {
+        // Utility class
+    }
+
+    /**
+     * Main method.
+     * @param args command line arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/lab1/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Lab 1 package.
+ */
+package com.lab1;
